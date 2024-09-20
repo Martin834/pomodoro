d248bf83d7565de09074a86f48cd40fb28928cc1
Martin <martin.bazoge@etu-unicaen.fr>
2024-09-20 13:34:25 +0200

    entierement fonctionnel manque le style

--- script.js
+++ script.js
@@ -1,41 +1,37 @@
 let m = 25
 let s = 0
+let travail = true
 
-function lanceTimerTravail() {
+let reset = document.createElement("button")
+reset.textContent = "RESET"
+reset.addEventListener("mousedown", resetPage)
+
+function lanceTimer() {
     document.getElementById("lance").remove()
     document.body.appendChild(reset)
     document.getElementById("moment_travail").textContent = "ALLER AU BOULOT PETITE LARVE JE TE SURVAILLE"
-    const timer = setInterval(function() {
-        document.getElementById("minutes").textContent = m
-        document.getElementById("secondes").textContent = s
-        if (m == 0 && s == 0) {
-            document.getElementById("moment_pause").textContent = "VA EN PAUSE LA"
-            document.getElementById("moment_travail").textContent = ""
-            m = 5
-            s = 0
-            lanceTimerPause()
-        }
-        else if (s == 0) {
-            m--
-            s = 59
-        }
-        else {
-            s--
-        }
-    }, 1)
-}
 
-function lanceTimerPause() {
     const timer = setInterval(function() {
         document.getElementById("minutes").textContent = m
         document.getElementById("secondes").textContent = s
+
         if (m == 0 && s == 0) {
-            document.getElementById("moment_travail").textContent = "ALLER AU BOULOT PETITE LARVE JE TE SURVAILLE"
-            document.getElementById("moment_pause").textContent = ""
-            m = 25
-            s = 0
-            lanceTimerTravail()
+            if (travail == true) {
+                travail = false
+                document.getElementById("moment_pause").textContent = "VA EN PAUSE LA"
+                document.getElementById("moment_travail").textContent = ""
+                m = 5
+                s = 0
+            }
+            else {
+                travail = true
+                document.getElementById("moment_travail").textContent = "ALLER AU BOULOT PETITE LARVE JE TE SURVAILLE"
+                document.getElementById("moment_pause").textContent = ""
+                m = 25
+                s = 0
+            }
         }
+
         else if (s == 0) {
             m--
             s = 59
