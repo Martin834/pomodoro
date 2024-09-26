72334598c0b8bc434ecc6c5d37ad8795d28c51a4
Martin Bazoge <martin.bazoge@etu.unicaen.fr>
2024-09-26 13:43:49 +0200

    site entierement fonctionnel et background responsive

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-let m_travail
-let s_travail
-let m_pause
-let s_pause
+let m = 25
+let s = 0
+
+let m_travail = 25
+let s_travail = 0
+let m_pause = 5
+let s_pause = 0
 let travail = true
 
 let alerte = new Audio('sons/alerte.mp3')
@@ -12,7 +15,10 @@ function lanceTimer() {
 
     document.getElementById("moment_travail").textContent = "Travaille bien cher ami"
 
+    
+
     const timer = setInterval(function() {
+        
         document.getElementById("minutes").textContent = m
         document.getElementById("secondes").textContent = s
 
@@ -21,16 +27,16 @@ function lanceTimer() {
                 travail = false
                 document.getElementById("moment_pause").textContent = "C'est la pause !"
                 document.getElementById("moment_travail").textContent = ""
-                m = 5
-                s = 0
+                m = m_pause
+                s = s_pause
                 alerte.play()
             }
             else {
                 travail = true
                 document.getElementById("moment_travail").textContent = "Allez hop hop au boulot !"
                 document.getElementById("moment_pause").textContent = ""
-                m = 25
-                s = 0
+                m = m_travail
+                s = s_travail
                 alerte.play()
             }
         }
@@ -42,7 +48,7 @@ function lanceTimer() {
         else {
             s--
         }
-    }, 1)
+    }, 1000)
 }
 
 function resetPage() {
@@ -51,50 +57,55 @@ function resetPage() {
 
 function changeTemps(nb_secondes, travail) {
     if (travail == true) {
-        m = document.getElementById("set_minutes_travail").textContent
-        s = document.getElementById("set_secondes_travail").textContent
+        m_travail = document.getElementById("set_minutes_travail").textContent
+        s_travail = document.getElementById("set_secondes_travail").textContent
         if (nb_secondes < 0) {
-            if (s == 0) {
-                s = 30
-                m--
+            if (s_travail == 0) {
+                s_travail = 30
+                m_travail--
             }
             else {
-                s = 0
+                s_travail = 0
             }
         }
         else {
-            if (s == 30) {
-                s = 0
-                m++
+            if (s_travail == 30) {
+                s_travail = 0
+                m_travail++
             }
             else {
-                s = 30
+                s_travail = 30
             }
         }
-        document.getElementById("set_minutes_travail").textContent = m
-        document.getElementById("set_secondes_travail").textContent = s
+        document.getElementById("set_minutes_travail").textContent = m_travail
+        document.getElementById("set_secondes_travail").textContent = s_travail
+        document.getElementById("minutes").textContent = m_travail
+        document.getElementById("secondes").textContent = s_travail
     }
     else {
-        m = document.getElementById("set_secondes_pause").textContent
-        s = document.getElementById("set_secondes_pause").textContent
+        m_pause = document.getElementById("set_minutes_pause").textContent
+        s_pause = document.getElementById("set_secondes_pause").textContent
         if (nb_secondes < 0) {
-            if (s == 0) {
-                s = 30
-                m--
+            if (s_pause == 0) {
+                s_pause = 30
+                m_pause--
             }
             else {
-                s = 0
+                s_pause = 0
             }
         }
         else {
-            if (s == 30) {
-                s = 0
-                m++
+            if (s_pause == 30) {
+                s_pause = 0
+                m_pause++
             }
             else {
-                s = 30
+                s_pause = 30
             }
         }
+        document.getElementById("set_minutes_pause").textContent = m_pause
+        document.getElementById("set_secondes_pause").textContent = s_pause
     }
-
+    m = m_travail
+    s = s_travail
 }
